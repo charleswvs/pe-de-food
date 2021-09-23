0a90eb5e05c08447d7f5209660ce9d061a06a4ff
cissa-fr-dev <ceciliafr.contato@gmail.com>
2021-09-22 21:15:44 -0300

    pratos para você

--- src/pages/Pratos/index.jsx
+++ src/pages/Pratos/index.jsx
@@ -1,7 +1,38 @@
-export const Pratos = () => {
+import React from 'react';
+import './styles.css';
+
+const Pratos = (pratos) => {
   return (
-    <>
-     <h1> tela de pratos</h1>
-    </>
-  )
-}
+    <div className="container">
+      <h3 className="title">Pratos para você</h3>
+      <div className="top">
+        <div className="cardContainer">
+          <div className="card"></div>
+          <span className="text">Panqueca doce</span>
+        </div>
+        <div className="cardContainer">
+          <div className="card"></div>
+          <span className="text">Panqueca doce</span>
+        </div>
+        <div className="cardContainer">
+          <div className="card"></div>
+          <span className="text">Panqueca doce</span>
+        </div>
+        <div className="cardContainer">
+          <div className="card"></div>
+          <span className="text">Panqueca doce</span>
+        </div>
+        <div className="cardContainer">
+          <div className="card"></div>
+          <span className="text">Panqueca doce</span>
+        </div>
+        <div className="cardContainer">
+          <div className="card"></div>
+          <span className="text">Panqueca doce</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pratos;
