4f3dcc1f21f54457c87ee86c552b89e308116b23
Charles Willian <wcharlesw15@gmail.com>
2021-09-30 22:41:37 -0300

    feature: tela de restaurante

--- src/pages/Restaurante/index.jsx
+++ src/pages/Restaurante/index.jsx
@@ -1,12 +1,31 @@
+import './restaurante.css';
 import React from 'react';
+import Produto from '../../components/produto';
 
 const Restaurante = () => {
   return (
-    <main>
-      <section>
-        <img src="" />
-      </section>
-    </main>
+    <div style={{backgroundColor: '#E0DDDC'}}>
+      <main className="restaurantContainer">
+        <section className="restaurantHeader">
+          <img alt="resturante do seu zé" src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80" />
+        </section>
+        <h1>
+          Recanto do seu zé - sanduíches artesanais
+        </h1>
+        <div className="restaurantInputContainer">
+          <input className="restaurantInput" placeholder="Digite o nome de um prato" />
+          <button className="restaurantSearchButton">
+            BUSCAR
+          </button>
+        </div>
+        <div className="restaurantGrid">
+          <Produto/>
+          <Produto/>
+          <Produto/>
+          <Produto/>
+        </div>
+      </main>
+    </div>
   )
 }
 
