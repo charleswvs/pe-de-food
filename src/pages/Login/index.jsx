38f3ea8e98346ae3483129a72b2d334118537269
jayv1s <jgoncalves@ciandt.com>
2021-11-11 19:01:28 -0300

    Criado a integracao da rota de login e cadastro com o BE

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,27 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import "./login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({email: "", password: ""});
+
+
+  const handleChangeEmail = event => {
+    setEmail(event.target.value);
+  }
+
+  const handleChangePassword = event => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const user = {
+      email: email,
+      password: password
+    };
+
+    axios.post(`http://localhost:8080/login`, user )
+      .then(res => {
+        console.log(res);
+        setUser(res.data)
+      })
+      // Como limpar input dps que submeter?
+  }
   return (
     <div className="container">
       <h1>Pé de food</h1>
       <section className="loginContainer">
         <h2>Login</h2>
 
-        <input
-          className="emailInput"
-          type="email"
-          placeholder="Digite seu e-mail"
-        />
-        <input
-          className="passwordInput"
-          type="password"
-          placeholder="Digite sua senha"
-        />
-
-        <Link className="register" to="/restaurantes">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="emailInput"
+            type="email"
+            onChange={handleChangeEmail}
+            placeholder="Digite seu e-mail"
+          />
+          <input
+            className="passwordInput"
+            type="password"
+            onChange={handleChangePassword}
+            placeholder="Digite sua senha"
+          />
           <button className="buttonEntrar">Entrar</button>
+        </form>
+
+        {/* Como fazer para, quando o usuário clicar em entrar, redirecionar para tela de restaurantes */}
+        <Link className="register" to="/restaurantes">
         </Link>
         <Link className="register" to="/cadastrar">
           Cadastrar
--- src/pages/Logon/index.jsx
+++ src/pages/Logon/index.jsx
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './logon.css';
 
 const Logon = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({email: "", password: ""});
+
+
+  const handleChangeEmail = event => {
+    setEmail(event.target.value);
+  }
+
+  const handleChangePassword = event => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const user = {
+      email: email,
+      password: password
+    };
+    console.log(user)
+
+    axios.post(`http://localhost:8080/cadastro`, user )
+      .then(res => {
+        console.log(res);
+        setUser(res.data)
+      })
+      // Como limpar input dps que submeter?
+  }
   return (
     <div className="container">
       <h1>Pé de food</h1>
       <section className="loginContainer">
         <h2>Cadastrar</h2>
 
-        <input
-          className="nameInput"
-          type="text"
-          placeholder="Digite seu nome"
-        />
-        <input
-          className="emailInput"
-          type="email"
-          placeholder="Digite seu e-mail"
-        />
-        <input
-          className="passwordInput"
-          type="password"
-          placeholder="Digite sua senha"
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            className="emailInput"
+            type="email"
+            onChange={handleChangeEmail}
+            placeholder="Digite seu e-mail"
+          />
+          <input
+            className="passwordInput"
+            type="password"
+            onChange={handleChangePassword}
+            placeholder="Digite sua senha"
+          />
 
-        <button className="buttonEntrar">Cadastrar</button>
+          <button className="buttonEntrar">Cadastrar</button>
+        </form>
+        {/* Forçar o usuário a voltar pra tela inicial */}
         <Link className="register" to="/">
           Voltar para o login
         </Link>
